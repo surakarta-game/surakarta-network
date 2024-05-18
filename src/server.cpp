8921f2074ad799b393edb0160bec3edea71a4fe8
nictheboy <nictheboy@outlook.com>
2024-05-18 23:37:21 +0800

    feat: custom port in server

--- src/server.cpp
+++ src/server.cpp
@@ -17,15 +17,20 @@ void onSignal(int signal) {
     }
 }
 
-int main() {
-    int port = 7777;
-    auto logger = std::make_shared<SurakartaLoggerStdout>();
-    NetworkFramework::Server server(std::make_shared<SurakartaNetworkServiceFactory>(), port);
-    signal(SIGINT, onSignal);
+int main(int argc, char** argv) {
+    if (argc > 1) {
+        int port = std::stoi(argv[1]);
+        auto logger = std::make_shared<SurakartaLoggerStdout>();
+        NetworkFramework::Server server(std::make_shared<SurakartaNetworkServiceFactory>(), port);
+        signal(SIGINT, onSignal);
 
-    std::unique_lock lock(mutex);
-    condition_variable.wait(lock, [&] { return !running; });
+        std::unique_lock lock(mutex);
+        condition_variable.wait(lock, [&] { return !running; });
 
-    logger->Log("Server is shutting down...");
-    return 0;
+        logger->Log("Server is shutting down...");
+        return 0;
+    } else {
+        printf("Usage: %s <port>\n", argv[0]);
+        return 1;
+    }
 }
