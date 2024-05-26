3d9b6400636fafa73e96f34ef78274bb2de2acfd
nictheboy <nictheboy@outlook.com>
2024-05-26 23:26:03 +0800

    fix: not standard path

--- submission/Client_Team_3.cpp
+++ submission/Client_Team_3.cpp
@@ -7,19 +7,25 @@
 #include "surakarta_network.h"
 
 int main(int argc, char* argv[]) {
-    if (argc < 2) {
-        std::cerr << "Usage: " << argv[0] << " <server_address> <server_port>" << std::endl;
+    if (argc < 4) {
+        std::cerr << "Usage: " << argv[0] << " <server_address> <server_port> <color>" << std::endl;
         return 1;
     }
 
     std::string server_address = argv[1];
     int server_port = std::stoi(argv[2]);
+    auto color_str = std::string(argv[3]);
+    auto color = (color_str == "BLACK" || color_str == "black")
+                     ? PieceColor::BLACK
+                 : (color_str == "WHITE" || color_str == "white")
+                     ? PieceColor::WHITE
+                     : PieceColor::NONE;
 
     int reverse_proxy_port = 30000 + GlobalRandomGenerator::getInstance()() % 10000;
-    auto path = std::filesystem::current_path() / ".." / "record" / "client";
-    std::filesystem::create_directory(std::filesystem::current_path() / ".." / "record");
-    std::filesystem::create_directory(std::filesystem::current_path() / ".." / "record" / "client");
-    auto run_log_path = path / (std::to_string(GlobalRandomGenerator::getInstance()()) + ".log");
+    auto path = std::filesystem::current_path() / "record" / "client";
+    std::filesystem::create_directory(std::filesystem::current_path() / "record");
+    std::filesystem::create_directory(std::filesystem::current_path() / "record" / "client");
+    auto run_log_path = path / ("Team_3_" + std::to_string(GlobalRandomGenerator::getInstance()()) + ".log");
     auto reverse_proxy_server = NetworkFramework::Server(
         std::make_shared<ReverseProxyService>(
             server_address,
@@ -33,7 +39,7 @@ int main(int argc, char* argv[]) {
         reverse_proxy_port,
         "client",
         0,
-        PieceColor::NONE,
+        color,
         std::make_shared<SurakartaLogger>(
             std::make_shared<SurakartaLoggerStreamMultiple>(
                 std::make_shared<SurakartaLoggerStreamStdout>(),
